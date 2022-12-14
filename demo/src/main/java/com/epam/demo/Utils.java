f34ace29e93c2beb2038171abdc018ebed8fc32e
Darkhan Zhursin <darkhan.zhursin@mediamonks.com>
2022-12-14 18:11:09 +0600

    Task #2. Build Tools. Maven

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -2,8 +2,20 @@ package com.epam.demo;
 
 import java.util.List;
 
+import com.epam.utils.StringUtils;
+
+
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
        //magic happens here
+        if (!args.isEmpty() || args.size() > 0) {
+            for (int i = 0; i < args.size(); i++) {
+                if (StringUtils.isPositiveNumber(args.get(i)) == false) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,18 @@
 package com.epam.utils;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
         //code!
+        if (NumberUtils.isCreatable(str)) {
+            double res = NumberUtils.toDouble(str);
+            if (res > 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
     }
 }
